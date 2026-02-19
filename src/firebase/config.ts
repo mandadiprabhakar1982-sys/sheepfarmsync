07d7e47b791e91efab918227f56eea984eed48ac
Prabhakar Reddy Mandadi <mprabhakar99@gmail.com>
2026-02-19 13:59:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7371766246-d12c8",
+  "appId": "1:940227476598:web:f77ccfac1aaa28db743357",
+  "apiKey": "AIzaSyBoCVlVXKyvX7Dk1V24IX9TQK0o9jhIDks",
+  "authDomain": "studio-7371766246-d12c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "940227476598"
+};
